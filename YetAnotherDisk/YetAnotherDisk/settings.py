6d834008911c6bf7659e010819ece73ac3fdf021
Руслан Николаев <rusnikolaev1@gmail.com>
2022-09-15 22:19:49 +0500

    almost done 2

--- YetAnotherDisk/YetAnotherDisk/settings.py
+++ YetAnotherDisk/YetAnotherDisk/settings.py
@@ -73,13 +73,6 @@ DATABASES = {
     }
 }
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}"""
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -116,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
